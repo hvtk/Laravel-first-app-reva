113299ac844cc84eaeaf914e530c09b7bd0416f4
hvtk <hvtk1968@gmail.com>
2021-12-09 16:11:27 +0100

    setup url routes

--- web.php
+++ web.php
@@ -21,15 +21,29 @@ Route::get('/about', function() {
     return '<h4> the about page </h4>';
 });
 
-Route:: get('/store', function () {
-    $category = request('category');
-
-
-    //strip_tags voorkomt foutmeldingen door niet geprogrammeerde request-namen
+Route:: get('/store/{category?}/{item?}', function ($category = null, $item = null) {
     if (isset($category)) {
+
+        if (isset($item)) {
+            return "you are vieuwing ther store for  {$category} for {$item}";
+        }
+
         return 'you are viewing the store for ' . strip_tags($category);
     }
 
     return 'you are viewing all instruments.';
 
 });
+
+// Route:: get('/store', function () {
+//     $category = request('category');
+
+
+//     //strip_tags voorkomt foutmeldingen door niet geprogrammeerde request-namen
+//     if (isset($category)) {
+//         return 'you are viewing the store for ' . strip_tags($category);
+//     }
+
+//     return 'you are viewing all instruments.';
+
+// });
