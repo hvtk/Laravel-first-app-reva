d0774941b2b73751c922f7964d1f78d473750e14
hvtk <hvtk1968@gmail.com>
2021-12-09 15:44:26 +0100

    first setup routes

---
+++ web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return '<h1> hello world </h1>'; //view('welcome');
+});
+
+Route::get('/about', function() {
+    return '<h4> the about page </h4>';
+});
+
+Route:: get('/store', function () {
+    $category = request('category');
+
+
+    //strip_tags voorkomt foutmeldingen door niet geprogrammeerde request-namen
+    if (isset($category)) {
+        return 'you are viewing the store for ' . strip_tags($category);
+    }
+
+    return 'you are viewing all instruments.';
+
+});
