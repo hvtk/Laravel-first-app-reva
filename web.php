e0480ae7a57e8c4bd4ea2f7b4ce6cd6e01fc5fca
hvtk <hvtk1968@gmail.com>
2021-12-13 16:32:22 +0100

    All code till less 3.5

--- web.php
+++ web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\GuitarsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,9 +15,11 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get('/',[HomeController::class, 'index']);
-Route::get('/about',[HomeController::class, 'about']);
-Route::get('/contact',[HomeController::class, 'contact']);
+Route::get('/',[HomeController::class, 'index'])->name('home.index');
+Route::get('/about',[HomeController::class, 'about'])->name('home.about');
+Route::get('/contact',[HomeController::class, 'contact'])->name('home.contact');
+
+Route::resource('guitars', GuitarsController::class);
 
 Route:: get('/store/{category?}/{item?}', function ($category = null, $item = null) {
     if (isset($category)) {
